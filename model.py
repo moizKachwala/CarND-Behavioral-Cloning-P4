7fded1454ac39dd21dc2c62fbbc0bb7d2b0683bf
moizKachwala <moizk@hotmail.com>
2019-02-25 17:06:59 +0000

    added basic structure

---
+++ model.py
@@ -0,0 +1,50 @@
+import csv
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+lines = []
+with open('data/driving_log.csv') as csvfile:
+  reader = csv.reader(csvfile)
+  for line in reader:
+    lines.append(line)
+
+
+images = []
+measurements = []
+
+for line in lines[1:]:
+  source_path = line[0]
+  filename = source_path.split('/')[-1]
+  current_path = 'data/IMG/' + filename
+  image = mpimg.imread(current_path)
+  images.append(image)
+  measurement = float(line[3])
+  measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D, Cropping2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24,5,5, subsample=(2, 2),input_shape=(66,200,3),  border_mode='valid',activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2, 2), border_mode='valid',activation="relu"))
+model.add(Convolution2D(48,5,5, subsample=(2, 2), border_mode='valid',activation="relu"))
+model.add(Convolution2D(64,3,3, border_mode='valid',activation="relu"))
+model.add(Convolution2D(64,3,3, border_mode='valid',activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+
+model.save('model.h5')
